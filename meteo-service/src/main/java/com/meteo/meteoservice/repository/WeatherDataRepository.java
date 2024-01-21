e6b4657d8ff3e8f142baf75a4fa639e21445f1c6
Reda <126523551+boufi9@users.noreply.github.com>
2024-01-21 05:55:59 +0100

    Delete meteo-service directory

--- meteo-service/src/main/java/com/meteo/meteoservice/repository/WeatherDataRepository.java
+++
@@ -1,8 +0,0 @@
-package com.meteo.meteoservice.repository;
-
-import com.meteo.meteoservice.model.WeatherData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WeatherDataRepository extends JpaRepository<WeatherData, Long> {
-    // Méthodes personnalisées si nécessaire
-}
