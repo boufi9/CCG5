8944c486a7793d72d71b0e10c797dc6603f810cc
Reda <126523551+boufi9@users.noreply.github.com>
2024-01-21 05:56:08 +0100

    Delete notification-service directory

--- notification-service/src/main/java/com/meteo/notificationservice/repository/WeatherAlertRepository.java
+++
@@ -1,8 +0,0 @@
-package com.meteo.notificationservice.repository;
-
-import com.meteo.notificationservice.model.WeatherAlert;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WeatherAlertRepository extends JpaRepository<WeatherAlert, Long> {
-    // Méthodes personnalisées si nécessaire
-}
